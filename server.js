000612ad2c09b640de96f04e130faec778358a8f
kurkUma715 <kurkom4@gmail.com>
2026-02-13 23:12:19 +0800

    404, projects html

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const server = http.createServer((req, res) => {
-  console.log(`Request: ${req.url}`);
+  /* console.log(`Request: ${req.url}`); */
   let filePath = '.' + req.url;
   if (filePath === './') {
     filePath = './index.html'; 
